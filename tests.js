5e2fcb2515a0ff0b5d3df2952152e21855d0efcf
Jared Rice Sr <39349252+JaredRiceSr@users.noreply.github.com>
2018-05-21 10:07:40 -0500

    Update tests.js

--- tests.js
+++ tests.js
@@ -10,55 +10,55 @@ test("kID.fromSeed(kSeed)", function(t){
         t.deepEquals(r, expected);
     };
 
-    tst("3b75434f4fb431bfcdd1d7f3e0544fde12e5bb7d19b36a7f6cffe1a0bf1fd8ff", {
+    tst("a95e58d4b13ae0904f216d7b6c88dbfd786d7c860476769fccc78a56b7168711", {
 
-        kID: "jbJWZXeXSsD5o1iWhYGFc",
-        kVKey: "QDQ7Y69yg6eeJNstq62rXw8mK8HmnnsocPwvo9DU2tS",
-        uniKey: "C2CLejK2c3SC9Rz4JLFuNtWJM8WDRq2CEMJuRqaJvr35",
+        kID: "4jifBtJ25m8jizVRzoAGS2",
+        kVKey: "337tgZ21yZDNCW2iYkDH53fFM7twpZxJEw677eu1VFRA",
+        uniKey: "AuHAYMqugn1oq6YKcpAMcuY3pguz8imt6bZMeSDkeBKt",
 
         secret: {
-            kSeed: "3b75434f4fb431bfcdd1d7f3e0544fde12e5bb7d19b36a7f6cffe1a0bf1fd8ff",
-            kSigKey: "516mChDX1BRjwHJc2w838W8cXxy8a6Eb35HKXjPR2fD8",
-            secKey: "516mChDX1BRjwHJc2w838W8cXxy8a6Eb35HKXjPR2fD8",
+            kSeed: "a95e58d4b13ae0904f216d7b6c88dbfd786d7c860476769fccc78a56b7168711",
+            kSigKey: "CQ9Lq6R4iFVzKnjTSJUivBFtvyE7JXTKQKdDJVVN5HXN",
+            secKey: "CQ9Lq6R4iFVzKnjTSJUivBFtvyE7JXTKQKdDJVVN5HXN",
         },
     });
 
-    tst("5d40a10e4723a41216f98a3cc4c1082a4fa51a0ffa6196960ad931efe654dd2e", {
+    tst("8fab3b922700b431ddc41216a1abc98ab666b77da36d95ed70d7f78ce7229dc1", {
 
-        kID: "E7cwAzuB9kSU3mj2n9p97P",
-        kVKey: "89ZgeKmTktxWg9UrGFzL2PbcHrToKNMPpLQtjbh17pfT",
-        uniKey: "5UAXeov4Gi7ioSTLDoMPtdvqX6RRmJcQAWagVgdaxUej",
+        kID: "6KqxGE5L3nSjgXZdgwY6si",
+        kVKey: "3uLAkanKNzXGhz22juSiKJVEs1MbcdHuRELV9rnaRjwb",
+        uniKey: "Dm5KMsm79RHiGKJAZhnyW1B8fE6cz92dFAWgowZ3UJ8v",
 
         secret: {
-            kSeed: "5d40a10e4723a41216f98a3cc4c1082a4fa51a0ffa6196960ad931efe654dd2e",
-            kSigKey: "7H25Jfb2ND51hhaomL5FPhhqQvBGujd1jJeSjZZ8HQzR",
-            secKey: "7H25Jfb2ND51hhaomL5FPhhqQvBGujd1jJeSjZZ8HQzR",
+            kSeed: "8fab3b922700b431ddc41216a1abc98ab666b77da36d95ed70d7f78ce7229dc1",
+            kSigKey: "Afpjuqz38PjtbusCsZvkRJdSbByZnvV26EbKKQPfbF84",
+            secKey: "Afpjuqz38PjtbusCsZvkRJdSbByZnvV26EbKKQPfbF84",
         },
     });
 
-    tst("7cb46171cd7a9bf9440e9c74c0752b37ae89b72184bb6693d8dc73752078ea0f", {
+    tst("95ccb459d36317f64611319b4b202e3d862214ac010f5a2cd86ff9b48acd1d80", {
 
-        kID: "TvqM6vQHEZb4EDYkfUVVUp",
-        kVKey: "FgFdznhQTymQEBKNoboDLKDGWB7eezrhvKKQK2uKUSU5",
-        uniKey: "3mosoLnk91yNrGga3vJtLaFNXf9yi85gSNisMT643HyH",
+        kID: "V6sM3cECR2YHGALtv7E8uf",
+        kVKey: "GKLLoefDxVEK6UKpmKMJ9UwNALFEBhaA4JcSPM3vRosm",
+        uniKey: "HENTdXzShzPGMgaukyNhgqtvGJgoFQS9phZy9Y5CLFBg",
 
         secret: {
-            kSeed: "7cb46171cd7a9bf9440e9c74c0752b37ae89b72184bb6693d8dc73752078ea0f",
-            kSigKey: "9Po5sqUto67MYFyfXXgV3PwvXoRxCfEXpSoMKn1eFtcv",
-            secKey: "9Po5sqUto67MYFyfXXgV3PwvXoRxCfEXpSoMKn1eFtcv",
+            kSeed: "95ccb459d36317f64611319b4b202e3d862214ac010f5a2cd86ff9b48acd1d80",
+            kSigKey: "B5knk43jCGberzaUwkn5PJ7jY8XRpPk5BZHhuBMdprzT",
+            secKey: "B5knk43jCGberzaUwkn5PJ7jY8XRpPk5BZHhuBMdprzT",
         },
     });
 
-    tst("35604fb84e67d18a76b956d1cbf9ba7384994c3fba1e140ba95928cc98823058", {
+    tst("337765fd4629cd4955f13605851fd2faaca57a3c4da0051da3ca3fbe06f5125a", {
 
-        kID: "MAF6ioWmybYvjYU2HD9oBE",
-        kVKey: "BzH5a2wLEyKxySUALpfBiBjHZtZudCG68J17QwWkRsdN",
-        uniKey: "7QhcMiFkfZLf6TScAucX2kw3A9561MHMukWUhnsSzba8",
+        kID: "PVCYTeW3mecQsfNcNFFjPv",
+        kVKey: "DFqJVtSdMtAJP29LQ9vPjZbbt1Z2if71H7nMAMcKkmHs",
+        uniKey: "Eivy5nCEe9BZqd8a9yidBGDLAvrvdMJSUYKU5SWCmC4X",
 
         secret: {
-            kSeed: "35604fb84e67d18a76b956d1cbf9ba7384994c3fba1e140ba95928cc98823058",
-            kSigKey: "4bMnc36WuLYJqsWTZtiazJJrtkvPwgyWnirn7gKk7ium",
-            secKey: "4bMnc36WuLYJqsWTZtiazJJrtkvPwgyWnirn7gKk7ium",
+            kSeed: "337765fd4629cd4955f13605851fd2faaca57a3c4da0051da3ca3fbe06f5125a",
+            kSigKey: "4TuPa3QgeKmChACiQYWv6JkQWD9HDu78BFqirGaVt3iq",
+            secKey: "4TuPa3QgeKmChACiQYWv6JkQWD9HDu78BFqirGaVt3iq",
         },
     });
 
@@ -81,9 +81,9 @@ test("kID.goKep()", function(t){
 
 test("kID.signMessage(message, kSigKey, kVKey)", function(t) {
 
-    var kSigKey = "4bMnc36WuLYJqsWTZtiazJJrtkvPwgyWnirn7gKk7ium";
-    var kVKey = "BzH5a2wLEyKxySUALpfBiBjHZtZudCG68J17QwWkRsdN";
-    var message = "Hello World!!";
+    var kSigKey = "8F6Ub3U7a7sG14mGftYXcwAYU1h7x3KEaxjwLRTpwhp9";
+    var kVKey = "CAvg8J3mR73uYMbwzxmsvHwBPfEDXqBMWhnLZ9fhBNWx";
+    var message = "One Major Step.";
 
     var signedMessage = kID.signMessage(message, kSigKey, kVKey);
     t.notEqual(message, signedMessage);
@@ -92,27 +92,27 @@ test("kID.signMessage(message, kSigKey, kVKey)", function(t) {
 
 test("kID.verifySignedMessage(signedMessage, kVKey)", function(t) {
 
-    var kSigKey = "4bMnc36WuLYJqsWTZtiazJJrtkvPwgyWnirn7gKk7ium";
-    var kVKey = "BzH5a2wLEyKxySUALpfBiBjHZtZudCG68J17QwWkRsdN";
-    var verifyKey2 = "QDQ7Y69yg6eeJNstq62rXw8mK8HmnnsocPwvo9DU2tS";
-    var kSigKey2 = "516mChDX1BRjwHJc2w838W8cXxy8a6Eb35HKXjPR2fD8";
-    var message = "Hello World!!";
-    var message2 = "I want to take over the world!!";
+    var kSigKey = "6i3CZj7MHrfuCJ3T1twdvWaKEkThAdFDxq6Tia1HCvWc";
+    var kVKey = "4AfKzGmumQhA87VuGmftJrsJVzFGutKRoWQsoxVmpLRY";
+    var kVKey2 = "34Nj6TtcUjknrBH2vuBeNdsrngm8PqHp2x336otsya2r";
+    var kSigKey2 = "8dchV2pWzR4g3SwYiaQyYbHN5qCFgQNLuMdSUwcPpGop";
+    var message = "One small step.";
+    var message2 = "For mankind.";
 
     var signedMessage = kID.signMessage(message, kSigKey, kVKey);
-    var signedMessage2 = kID.signMessage(message2, kSigKey2, verifyKey2);
+    var signedMessage2 = kID.signMessage(message2, kSigKey2, kVKey2);
 
     t.equal(kID.verifySignedMessage(signedMessage, kVKey), message);
-    t.equal(kID.verifySignedMessage(signedMessage, verifyKey2), false);
+    t.equal(kID.verifySignedMessage(signedMessage, kVKey2), false);
 
-    t.equal(kID.verifySignedMessage(signedMessage2, verifyKey2), message2);
+    t.equal(kID.verifySignedMessage(signedMessage2, kVKey2), message2);
     t.equal(kID.verifySignedMessage(signedMessage2, kVKey), false);
 
     t.end();
 });
 
 test("kID.getKeyPairFromSignKey(kSigKey)", function(t) {
-    var kSigKey = "4bMnc36WuLYJqsWTZtiazJJrtkvPwgyWnirn7gKk7ium";
+    var kSigKey = "Dsc4doSuoqTBUTuJuWGDSMeD2cG6fcSBNwwMRL6hHghi";
     var keyPair = kID.getKeyPairFromSignKey(kSigKey);
 
     t.equal(keyPair.publicKey.length, 32);
@@ -122,9 +122,9 @@ test("kID.getKeyPairFromSignKey(kSigKey)", function(t) {
 });
 
 test("kID.getSharedSecret(theirKVKey, myKSigKey)", function(t) {
-    var kSigKey1 = "4bMnc36WuLYJqsWTZtiazJJrtkvPwgyWnirn7gKk7ium";
-    var kSigKey2 = "516mChDX1BRjwHJc2w838W8cXxy8a6Eb35HKXjPR2fD8";
-    var kSigKey3 = "7H25Jfb2ND51hhaomL5FPhhqQvBGujd1jJeSjZZ8HQzR";
+    var kSigKey1 = "NfwG9qyKa4VEaf3ZXrsjTv7wBReoibWEPkMXhSkc1CX";
+    var kSigKey2 = "DM5Kp4RdmDCP5Gj9cEsdb79wDoQGr3ZaFTxtLXF97t26";
+    var kSigKey3 = "7LYwAW3HmZaB2ATsvYh2cxfwMQfsp9FcyCiy8p56KVCu";
 
     var keyPair1 = kID.getKeyPairFromSignKey(kSigKey1);
     var keyPair2 = kID.getKeyPairFromSignKey(kSigKey2);
@@ -150,8 +150,8 @@ test("kID.getSharedSecret(theirKVKey, myKSigKey)", function(t) {
 });
 
 test("kID.encryptMessage(message, kNonce, sharedSecret)", function (t) {
-    var kSigKey1 = "4bMnc36WuLYJqsWTZtiazJJrtkvPwgyWnirn7gKk7ium";
-    var kSigKey2 = "516mChDX1BRjwHJc2w838W8cXxy8a6Eb35HKXjPR2fD8";
+    var kSigKey1 = "3cXULEbf8eeVqnzHTACY7HaV1irxo5fdMVXUjgEcbcS8";
+    var kSigKey2 = "CHrWt1DVXqCNxJWWP7RhQBvcsp6MMLavaEmpC7aVFp1D";
 
     var keyPair1 = kID.getKeyPairFromSignKey(kSigKey1);
     var keyPair2 = kID.getKeyPairFromSignKey(kSigKey2);
@@ -168,9 +168,9 @@ test("kID.encryptMessage(message, kNonce, sharedSecret)", function (t) {
 });
 
 test("kID.decryptMessage(theirKVKey, myKSigKey", function(t) {
-    var kSigKey1 = "4bMnc36WuLYJqsWTZtiazJJrtkvPwgyWnirn7gKk7ium";
-    var kSigKey2 = "516mChDX1BRjwHJc2w838W8cXxy8a6Eb35HKXjPR2fD8";
-    var kSigKey3 = "7H25Jfb2ND51hhaomL5FPhhqQvBGujd1jJeSjZZ8HQzR";
+    var kSigKey1 = "6Z6cHSL6Y7TmaBHfQWVCiaLkN1EH9podPGfjT8wv7mEh";
+    var kSigKey2 = "1KfYMpmNMFMTWxT1NZFrTbrxQoyBEM4WGcD8Ha7V9K7";
+    var kSigKey3 = "ENQB8LowXPk1X3MNyLPDC98b6bd4BQ5Q3nwpWmTQ5gcE";
 
     var keyPair1 = kID.getKeyPairFromSignKey(kSigKey1);
     var keyPair2 = kID.getKeyPairFromSignKey(kSigKey2);
@@ -180,7 +180,7 @@ test("kID.decryptMessage(theirKVKey, myKSigKey", function(t) {
     var sharedSecret2To1 = kID.getSharedSecret(keyPair1.publicKey, keyPair2.secretKey);
     var sharedSecret3To1 = kID.getSharedSecret(keyPair3.publicKey, keyPair1.secretKey);
 
-    var message = "Hello World!!";
+    var message = "One small step.";
     var kNonce = kID.getKNonce();
 
     var encryptedMessage = kID.encryptMessage(message, kNonce, sharedSecret1To2);
